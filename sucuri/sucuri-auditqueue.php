06ba170e406acaeb5683fc11a1d9e44e8b8506d7
Jotadloor14 <jotadloor@gmail.com>
2026-05-23 12:12:30 -0500

    Add files via upload

---
+++ sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1777218212;
+// updated_on=1777218212;
+exit(0);
+?>
+1777218212_1386:"Depuraci\u00f3n: admin, 127.0.0.1; El plugin Sucuri se ha desinstalado"
---
+++ sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1777218212;
+// updated_on=1777218212;
+exit(0);
+?>
